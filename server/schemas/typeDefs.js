2fada7035995f1af810dae5bb2627f1fdd2a03c0
Mychael Contreras <miketw1212@hotmail.com>
2022-11-28 16:55:46 -0700

    condensed models updated resolvers and server.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,56 +6,27 @@ const typeDefs = gql`
     username: String
     email: String
     password: String
-    budget: [Budget]!
+    budgets: [Budget]!
   }
 
   type Budget {
     _id: ID
     name: String
-    expense: String
-    budget: [Budget]!
+    expense: Number!
+    budget: Number!
   }
 
-  type Home {
-    _id: ID
-    payment: Number
-  }
-
-  type Food {
-    grocery: String
-    expense: Number
-  }
-
-  type Utilities {
-    utilities: String
-    expense: Number
-  }
-
-  type Vehicle {
-    name: String
-    expense: Number
-  }
-
-  type Misc {
-    name: String
-    expense: Number
-  }
 
   type Query {
     users: [User]
     user(username: String!): User
-    thoughts(username: String): [Thought]
-    thought(thoughtId: ID!): Thought
+    budgets(_id: ID): [Budget]
     me: User
   }
 
   type Mutation {
-    addUser(username: String!, email: String!, password: String!): Auth
-    login(email: String!, password: String!): Auth
-    addThought(thoughtText: String!): Thought
-    addComment(thoughtId: ID!, commentText: String!): Thought
-    removeThought(thoughtId: ID!): Thought
-    removeComment(thoughtId: ID!, commentId: ID!): Thought
+    addBudget(name: String!, expense: Number!, budget: Number!): Budget
+    removeBudget(budgetid: ID): String
   }
 `;
 
