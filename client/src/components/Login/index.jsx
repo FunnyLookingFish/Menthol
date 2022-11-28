d2cc5547e57b57c8348d5ecacacd9b4dfffd2d01
Jake Schmidt <jakeschmidt51@gmail.com>
2022-11-28 13:46:51 -0700

    changes to login

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -41,17 +41,6 @@ export default function Login(){
               placeholder="Password"
             />
           </div>
-
-          <div class="flex justify-between items-center mb-6">
-            <div class="form-group form-check">
-              <input
-                type="checkbox"
-                class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                id="exampleCheck2"
-              />
-            </div>
-          </div>
-
           <div class="text-center lg:text-left">
             <button
               type="button"
@@ -64,7 +53,7 @@ export default function Login(){
               <a
                 href="#!"
                 class="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
-                >Register</a
+                >Sign Up</a
               >
             </p>
           </div>
