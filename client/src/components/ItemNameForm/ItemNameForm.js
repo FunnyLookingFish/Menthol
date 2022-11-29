5d8bd0279aae30c69b0ad9288eb10bd505d44c87
Morgan Tolman <morgan.tolman04@gmail.com>
2022-11-29 11:06:22 -0700

    Setup ItemNameForm component.

--- client/src/components/ItemNameForm/ItemNameForm.js
+++ client/src/components/ItemNameForm/ItemNameForm.js
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import { useFinanceContext } from "../../utils/stateManagment/GlobalState";
+import { useQuery, useMutation } from '@apollo/client'
 
 export default function ItemNameForm(){
     const [inputValue, setInputValue] = useState();
     const [optionValue, setOptionValue] = useState();
-    const [state, dispatch] = useFinanceContext();
+
+    const { loading, data } = useQuery();//search for categories
+    const [ addItemToCat, {error}] = useMutation();//add item to category
 
     function handleInputChange(e){
         setInputValue(e.target.value);
@@ -12,25 +15,23 @@ export default function ItemNameForm(){
 
     function handleFormSubmit(e){
         e.preventDefault();
-        dispatch({
-            type: 'ADD_ITEM_TO_CAT',
-            payload:{
-                item: [inputValue],
-                name: optionValue
-            }
-        })
+        addItemToCat({variables: {_id: optionValue, itemName: inputValue}});
     };
 
     function handleOptionChange(e){
-        setOptionValue(e.target.value)
+        setOptionValue(e.target.id);
     };
     
+    if(loading){
+        return <h2>Loading...</h2>
+    }
+
     return(
         <form onSubmit={handleFormSubmit}>
             <input placeholder="Item Name" value={inputValue} onChange={handleInputChange} />
             <label>Choose a category:
                 <select value={optionValue} onChange={handleOptionChange}>
-                    {state.categories.map((category)=> <option key={category.name} value={category.name}>{category.name}</option>)}
+                    {data.map((category)=> <option key={category.name} id={category._id} value={category.name}>{category.name}</option>)}
                 </select>
             </label>
             <button type="submit">Add item to list</button>
