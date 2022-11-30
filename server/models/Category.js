2c5f64c5edd5c6fb00fc3f510651803d57073be8
FunnyLookingFish <112431728+FunnyLookingFish@users.noreply.github.com>
2022-11-29 18:04:10 -0700

    added Category model

--- server/models/Category.js
+++ server/models/Category.js
@@ -1,5 +1,23 @@
 const { Schema, model } = require('mongoose');
 
-const categorySchema = new 
+const categorySchema = new Schema({
+    items: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    cost: {
+        type: Number,
+        required: true,
+        min: 0.01,
+    }
+})
+
+const Category = model('Category', categorySchema);
 
 module.exports = Category;
