f8fdc9f40f2c01e643777c35ef973d717482a17d
Jake Schmidt <jakeschmidt51@gmail.com>
2022-12-01 13:53:52 -0700

    a

--- client/src/pages/Budget.js
+++ client/src/pages/Budget.js
@@ -1,12 +1,14 @@
 import { useQuery } from "@apollo/client";
 import ExpenseTable from "../components/ExpenseTable/expenseTable";
-import Header from '../components'
+import Header from '../components/Header/header';
+import Footer from '../components/Footer/Footer'
 export default function Budget(){
-    const {data} = useQuery('expenses', )//expenses resolver goes here
+    const {data} = useQuery('expenses', )//expenses reducer goes here
     return (
         <div>
             <Header/>
             <ExpenseTable items={data} />
+            <Footer/>
         </div>
     ) 
 }
