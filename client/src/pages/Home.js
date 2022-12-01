bb90e07f70f81cea60575a41ae2c38c9938b5d6b
FunnyLookingFish <112431728+FunnyLookingFish@users.noreply.github.com>
2022-12-01 11:35:38 -0700

    added part of Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,22 @@
 
 export default function Home(){
     return (
-    <div>
-        <p>hi</p>
-    </div>
+        <main>
+            <h1>Menthol</h1>
+            <hr />
+            <p>
+                <b>A place too keep track of all your fresh spends.</b>
+            </p>
+            <div>
+                <div>
+                    <h2>Sign in to track your expenses</h2>
+                    <button className="btn" style={{ cursor:'pointer' }} type="log-in" onClick={}></button>
+                </div>
+                <div>
+                    <h2>Don't have an account? Sign up for free today!</h2>
+                    <button className="btn" style={{ cursor: 'pointer' }} type="sign-up"></button>
+                </div>
+            </div>
+        </main>
     )
 }
