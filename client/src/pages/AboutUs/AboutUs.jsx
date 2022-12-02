b1a7a14b61e1672a6c17bd608e55a7d5a43877a2
Morgan Tolman <morgan.tolman04@gmail.com>
2022-12-01 20:54:22 -0700

    Updated about us page

--- client/src/pages/AboutUs/AboutUs.jsx
+++ client/src/pages/AboutUs/AboutUs.jsx
@@ -1,4 +1,6 @@
-import morgan from '../../assets/images/Morgan.jpg'
+import morgan from '../../assets/images/Morgan.jpg';
+import leo from '../../assets/images/leo.png'
+import lu from '../../assets/images/lu.png'
 
 export default function AboutUs(){
 
@@ -12,30 +14,16 @@ export default function AboutUs(){
                 </div>
             </article>
             <article className="flex w-full">
-                <img className=' rounded-full' src={morgan} alt="Morgan Tolman" />
+                <img className=' rounded-full' src={leo} alt="Morgan Tolman" />
                 <div>
-                    <h2>Morgan Tolman</h2>
+                    <h2>Leo Roccaforte</h2>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt facilis eligendi, atque libero ipsum eaque quisquam sed dolorum eum explicabo hic animi officiis, aliquam beatae tempora voluptatum saepe aperiam ducimus.</p>
                 </div>
             </article>
             <article className="flex w-full">
-                <img className=' rounded-full' src={morgan} alt="Morgan Tolman" />
+                <img className=' rounded-full' src={lu} alt="Morgan Tolman" />
                 <div>
-                    <h2>Morgan Tolman</h2>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt facilis eligendi, atque libero ipsum eaque quisquam sed dolorum eum explicabo hic animi officiis, aliquam beatae tempora voluptatum saepe aperiam ducimus.</p>
-                </div>
-            </article>
-            <article className="flex w-full">
-                <img className=' rounded-full' src={morgan} alt="Morgan Tolman" />
-                <div>
-                    <h2>Morgan Tolman</h2>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt facilis eligendi, atque libero ipsum eaque quisquam sed dolorum eum explicabo hic animi officiis, aliquam beatae tempora voluptatum saepe aperiam ducimus.</p>
-                </div>
-            </article>
-            <article className="flex w-full">
-                <img className=' rounded-full' src={morgan} alt="Morgan Tolman" />
-                <div>
-                    <h2>Morgan Tolman</h2>
+                    <h2>Lu Morales</h2>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt facilis eligendi, atque libero ipsum eaque quisquam sed dolorum eum explicabo hic animi officiis, aliquam beatae tempora voluptatum saepe aperiam ducimus.</p>
                 </div>
             </article>
